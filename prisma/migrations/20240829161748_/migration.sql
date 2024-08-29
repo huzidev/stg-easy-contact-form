6c8b53fa4ee7e6bc3a28c78a560505bf3d362f69
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2024-08-29 21:26:02 +0500

    migration updated

---
+++ prisma/migrations/20240829161748_/migration.sql
@@ -0,0 +1,67 @@
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "shop" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "isOnline" BOOLEAN NOT NULL DEFAULT false,
+    "scope" TEXT,
+    "expires" TIMESTAMP(3),
+    "accessToken" TEXT NOT NULL,
+    "userId" BIGINT,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "email" TEXT,
+    "accountOwner" BOOLEAN NOT NULL DEFAULT false,
+    "locale" TEXT,
+    "collaborator" BOOLEAN DEFAULT false,
+    "emailVerified" BOOLEAN DEFAULT false,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Form" (
+    "id" SERIAL NOT NULL,
+    "shopify_url" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "heading" TEXT,
+    "description" TEXT,
+    "showTitle" BOOLEAN NOT NULL DEFAULT false,
+    "onlinePublish" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Form_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Field" (
+    "id" SERIAL NOT NULL,
+    "shopify_url" TEXT NOT NULL,
+    "fieldName" TEXT NOT NULL,
+    "fieldLabel" TEXT NOT NULL,
+    "placeholder" TEXT NOT NULL,
+    "defaultValue" TEXT NOT NULL,
+    "fieldType" TEXT NOT NULL,
+    "min" INTEGER,
+    "max" INTEGER,
+    "totalLines" INTEGER,
+    "isRequired" BOOLEAN NOT NULL DEFAULT false,
+    "formId" INTEGER NOT NULL,
+
+    CONSTRAINT "Field_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Option" (
+    "id" SERIAL NOT NULL,
+    "label" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "fieldId" INTEGER NOT NULL,
+
+    CONSTRAINT "Option_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Field" ADD CONSTRAINT "Field_formId_fkey" FOREIGN KEY ("formId") REFERENCES "Form"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Option" ADD CONSTRAINT "Option_fieldId_fkey" FOREIGN KEY ("fieldId") REFERENCES "Field"("id") ON DELETE CASCADE ON UPDATE CASCADE;
